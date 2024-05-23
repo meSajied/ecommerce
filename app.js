2743cfd147c5eb8e09cf8eb05d8487a86da04402
meSajied <sajiedshahyousuf@gmail.com>
2024-05-23 13:18:40 +0600

    update: rate limit

--- app.js
+++ app.js
@@ -23,12 +23,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(rateLimit);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/order', orderPlacementRouter);
+app.use('/order', rateLimit, orderPlacementRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
